6453fa5ee724b07343d2d31a78b131ddedaa6f3a
Nilton Kazuo Hasegawa Haji <105826619+hajikazuo@users.noreply.github.com>
2022-07-06 19:28:55 -0300

    Update Program.cs

--- Exercicio3 avaliacao logica de programacao/Program.cs
+++ Exercicio3 avaliacao logica de programacao/Program.cs
@@ -10,6 +10,7 @@ namespace Exercicio3_avaliacao_logica_de_programacao
     {
         static void Main(string[] args)
         {
+            //Nilton Kazuo Hasegawa Haji RA:6322507
             // Criar um programa para calcular e mostrar a área de 3 figuras geométricas diferentes, não vale quadrado e nem retângulo.
             // Observação: as figuras são de escolha livre, dá um Google se não lembrar como faz o cálculo de alguma delas.
             //Requisitos: Deve ser apresentada uma lista para o usuário com o nome das 3 figuras que ele pode escolher para calcular.
